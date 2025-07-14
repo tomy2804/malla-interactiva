2d6c1b9f31d4e562193af0acee84871c40e28a93
tomy2804 <tomy60478@gmail.com>
2025-07-13 22:07:12 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+    const materiasData = [
+      // Año 1, Semestre 1
+      { id: "tallerInicialCienciasExactas", nombre: "Taller Inicial orientado: Ciencias Exactas", año: 1, semestre: 1, cargaHorariaTotal: 48, tipo: "obligatoria", bloque: "Ciencias Básicas" },
+      { id: "tallerInicialMatematica", nombre: "Taller Inicial obligatorio del área matemática", año: 1, semestre: 1, cargaHorariaTotal: 48, tipo: "obligatoria", bloque: "Ciencias Básicas" },
+      { id: "tallerInicialLecturaEscritura", nombre: "Taller inicial común: taller de lectura y escritura", año: 1, semestre: 1, cargaHorariaTotal: 48, tipo: "obligatoria", bloque: "Ciencias y Tecnologías Complementarias" },
+      { id: "tallerDeTecnologia", nombre: "Taller de tecnología", año: 1, semestre: 1, cargaHorariaTotal: 24, tipo: "obligatoria", bloque: "Ciencias y Tecnologías Complementarias" },
+      { id: "tallerIntroduccionIngenieria", nombre: "Taller de introducción a la ingeniería", año: 1, semestre: 1, cargaHorariaTotal: 24, tipo: "obligatoria", bloque: "Ciencias y Tecnologías Complementarias" },
+
+      // Año 1, Semestre 2
+      { id: "problemasSocioeconomicosContemporaneos", nombre: "Problemas socioeconómicos contemporáneos", año: 1, semestre: 2, cargaHorariaTotal: 64, tipo: "obligatoria", bloque: "Ciencias y Tecnologías Complementarias" },
+      { id: "tallerLecturaEscrituraDisciplinas", nombre: "Taller de lectura y escritura en las disciplinas", año: 1, semestre: 2, cargaHorariaTotal: 32, tipo: "obligatoria", bloque: "Ciencias y Tecnologías Complementarias" },
+      { id: "elementosDeCalculo", nombre: "Elementos de cálculo", año: 1, semestre: 2, cargaHorariaTotal: 128, tipo: "obligatoria", bloque: "Ciencias Básicas" },
+      { id: "fundamentosDeProgramacion", nombre: "Fundamentos de programación", año: 1, semestre: 2, cargaHorariaTotal: 64, tipo: "obligatoria", bloque: "Ciencias Básicas" },
+      { id: "quimicaGeneral", nombre: "Química general", año: 1, semestre: 2, cargaHorariaTotal: 96, tipo: "obligatoria", bloque: "Ciencias Básicas" },
+      { id: "mediosDeRepresentacion", nombre: "Medios de representación", año: 1, semestre: 2, cargaHorariaTotal: 64, tipo: "obligatoria", bloque: "Ciencias Básicas" },
+
+      // Año 2, Semestre 1
+      { id: "fisicaInicial", nombre: "Física inicial", año: 2, semestre: 1, cargaHorariaTotal: 96, tipo: "obligatoria", bloque: "Ciencias Básicas" },
+      { id: "quimicaInorganica", nombre: "Química inorgánica", año: 2, semestre: 1, cargaHorariaTotal: 96, tipo: "obligatoria", bloque: "Tecnologías Básicas" },
+      { id: "elementosDeAlgebraLineal", nombre: "Elementos de álgebra lineal", año: 2, semestre: 1, cargaHorariaTotal: 96, tipo: "obligatoria", bloque:
